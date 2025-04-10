7f7b89404aed7a38c637f378b211951c66a9409e
bjorkmz1 <bjorkmz1@aalto.fi>
2025-04-10 17:38:18 +0300

    Add colorbars to graph visualizations.

--- vamsl/utils/visualize.py
+++ vamsl/utils/visualize.py
@@ -1,6 +1,7 @@
 import os
 import matplotlib.pyplot as plt
 import imageio
+import matplotlib as mpl
 
 
 def visualize_ground_truth(mat, size=4.0, graph_label='Ground truth $G^*$'):
@@ -9,11 +10,12 @@ def visualize_ground_truth(mat, size=4.0, graph_label='Ground truth $G^*$'):
     """
     plt.rcParams['figure.figsize'] = [size, size]
     fig, ax = plt.subplots(1, 1)
-    ax.matshow(mat, vmin=0, vmax=1)
+    im = ax.matshow(mat, vmin=0, vmax=1)
     plt.setp(ax.get_xticklabels(), visible=False)
     plt.setp(ax.get_yticklabels(), visible=False)
     ax.tick_params(axis='both', which='both', length=0)
     ax.set_title(f'{graph_label}', pad=10)
+    fig.colorbar(im, ax=ax, shrink=0.7)
     plt.show()
     return
 
@@ -26,7 +28,6 @@ def visualize_ground_truths(mats, size=4.0, columns=7,graph_label='Ground truth 
         return visualize_ground_truth(mats, graph_label=graph_label)
     elif len(mats.shape) == 3 and mats.shape[0] == 1:
         return visualize_ground_truth(mats[0], graph_label=graph_label)
-        
     cols = min(len(mats), columns)
     rows = 1 + ((len(mats)-1)//cols)
     plt.rcParams['figure.figsize'] = [cols*size, rows*size]
@@ -36,11 +37,14 @@ def visualize_ground_truths(mats, size=4.0, columns=7,graph_label='Ground truth 
     for n, mat in enumerate(mats):
         i,j = n//cols, n%cols
         axs[i,j].set_axis_on()
-        axs[i,j].matshow(mat, vmin=0, vmax=1)
+        im = axs[i,j].matshow(mat, vmin=0, vmax=1)
         plt.setp(axs[i,j].get_xticklabels(), visible=False)
         plt.setp(axs[i,j].get_yticklabels(), visible=False)
         axs[i,j].tick_params(axis='both', which='both', length=0)
         axs[i,j].set_title(f'{graph_label} {n+1}', pad=10)
+        #if j == cols-1:
+
+    fig.colorbar(im, ax=axs.ravel().tolist(), shrink=0.7)
     plt.show()
     return
 
@@ -58,21 +62,21 @@ def visualize(mats, t, save_path=None, n_cols=7, size=2.5, show=False):
         n_rows += 1
     
     plt.rcParams['figure.figsize'] = [size * n_cols, size * n_rows]
-    fig, axes = plt.subplots(n_rows, n_cols)
-    axes = axes.flatten()
+    fig, axs = plt.subplots(n_rows, n_cols)
+    axes = axs.flatten()
 
     # for j, (ax, mat) in enumerate(zip(axes[:len(mats)], mats)):
     for j, ax in enumerate(axes): 
         if j < len(mats):
             # plot matrix of edge probabilities
-            ax.matshow(mats[j], vmin=0, vmax=1)
+            im = ax.matshow(mats[j], vmin=0, vmax=1)
             ax.tick_params(axis='both', which='both', length=0)
             ax.set_title(r'$Z^{('f'{j}'r')}$', pad=3)
         plt.setp(ax.get_xticklabels(), visible=False)
         plt.setp(ax.get_yticklabels(), visible=False)
         ax.axis('off')
-    
 
+    fig.colorbar(im, ax=axs.ravel().tolist(), shrink=0.7)
     # save
     if save_path is not None:
         if not os.path.exists(save_path):
